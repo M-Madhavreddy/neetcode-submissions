5ee93471efc949c040acd7d8cb4724296d36505b
M Madhav Reddy <98480251+M-Madhavreddy@users.noreply.github.com>
2026-07-29 10:21:21 +0530

    Add: ipo - submission-2

---
+++ Data Structures & Algorithms/ipo/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        #will take minHeap for capital and add capital, profit so when ever we get new
+        #capital we now pop the capital and check eligibilty and add that to maxheap
+        #which is holding profits
+
+        capheap,profHeap = [],[]
+
+        for cap,profit in zip(capital,profits):
+            heapq.heappush(capheap,(cap,profit))
+
+        totalCapital=0
+        
+        for i in range(k):
+            while capheap and capheap[0][0]<=w:
+                cap,profit = heapq.heappop(capheap)
+                heapq.heappush(profHeap,-1*profit)
+            
+            if profHeap:
+                w+= -1*heapq.heappop(profHeap)
+
+        return w;
+                
+            
+            
+
+
